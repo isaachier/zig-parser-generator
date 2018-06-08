e3339afbfb12e7a55b2f2af27c59d61c64e102a1
Isaac Hier <isaachier@gmail.com>
2018-06-08 08:51:17 -0400

    Improve tokenizer

--- src/parse.zig
+++ src/parse.zig
@@ -17,20 +17,56 @@ pub fn parseInputFile(allocator: *Allocator, path: []const u8) !void {
     var parser = Parser.init(allocator, &adapter);
     defer parser.deinit();
     var token = try parser.tokenizer.nextToken();
-    while (token.len() > 0) : (token = try parser.tokenizer.nextToken()) {
-        std.debug.warn("token: \"{}\"\n", token.toSliceConst());
+    while (token.kind != Token.Kind.EndOfFile) {
+        std.debug.warn("token: {} \"{}\"\n", @tagName(token.kind), if (!token.buffer.isNull() and token.buffer.len() > 0) token.buffer.toSliceConst() else "");
         token.deinit();
+        token = try parser.tokenizer.nextToken();
     }
+    token.deinit();
 }
 
+const Token = struct {
+    pub const Kind = enum {
+        Symbol,
+        String,
+        Equal,
+        Or,
+        EndOfRule,
+        EndOfFile,
+        Invalid,
+    };
+
+    kind: Kind,
+    buffer: Buffer,
+
+    pub fn init(allocator: *Allocator) Token {
+        return Token{
+            .kind = Kind.Invalid,
+            .buffer = Buffer.initNull(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Token) void {
+        self.buffer.deinit();
+    }
+
+    pub fn setBuffer(self: *Token, buffer: *Buffer) !void {
+        self.buffer = try Buffer.fromOwnedSlice(self.buffer.list.allocator, buffer.toOwnedSlice());
+    }
+};
+
 const Tokenizer = struct {
+    const ErrorSet = error {
+        OutOfMemory,
+    };
+
     in: *FileInStream,
-    lookahead: Buffer,
+    lookahead: Token,
 
     pub fn init(alloc: *Allocator, in: *FileInStream) Tokenizer {
         return Tokenizer{
             .in = in,
-            .lookahead = Buffer.initNull(alloc),
+            .lookahead = Token.init(alloc),
         };
     }
 
@@ -39,38 +75,64 @@ const Tokenizer = struct {
     }
 
     fn allocator(self: *Tokenizer) *Allocator {
-        return self.lookahead.list.allocator;
+        return self.lookahead.buffer.list.allocator;
     }
 
-    pub fn nextToken(self: *Tokenizer) !Buffer {
-        if (!self.lookahead.isNull()) {
-            std.debug.assert(self.lookahead.len() > 0);
-            var token = try Buffer.fromOwnedSlice(
-                self.allocator(),
-                self.lookahead.toOwnedSlice(),
-            );
-            self.lookahead.deinit();
-            self.lookahead = Buffer.initNull(self.allocator());
+    pub fn nextToken(self: *Tokenizer) Tokenizer.ErrorSet!Token {
+        if (self.lookahead.kind != Token.Kind.Invalid) {
+            var token = self.lookahead;
+            self.lookahead = Token.init(self.allocator());
             return token;
         }
 
-        var token = try Buffer.init(self.allocator(), "");
-        while (true) {
-            var byte: u8 = self.in.stream.readByte() catch return token;
+        var token = Token.init(self.allocator());
+        try token.buffer.resize(0);
+        loop: {
+            while (true) {
+                var byte: u8 = self.in.stream.readByte() catch {
+                    if (token.buffer.len() > 0) {
+                        break :loop;
+                    }
 
-            if (byte == ' ') {
-                return token;
+                    token.deinit();
+                    token = Token.init(self.allocator());
+                    token.kind = Token.Kind.EndOfFile;
+                    break :loop;
+                };
+
+                if (byte == ' ') {
+                    break :loop;
+                }
+
+                if (mem.indexOfScalar(u8, cstr.line_sep, byte) != null) {
+                    self.lookahead.kind = Token.Kind.EndOfRule;
+                    break :loop;
+                }
+
+                try token.buffer.appendByte(byte);
             }
-
-            if (mem.indexOfScalar(u8, cstr.line_sep, byte) != null) {
-                std.debug.assert(self.lookahead.isNull());
-                try self.lookahead.resize(1);
-                self.lookahead.list.items[0] = byte;
-                return token;
-            }
-
-            try token.appendByte(byte);
         }
+
+        if (token.kind == Token.Kind.Invalid) {
+            return switch (token.buffer.len()) {
+                1 => blk: {
+                    token.kind = switch (token.buffer.list.items[0]) {
+                        '"' => Token.Kind.String,
+                        '|' => Token.Kind.Or,
+                        '=' => Token.Kind.Equal,
+                        else => Token.Kind.Symbol,
+                    };
+                    break :blk token;
+                },
+                0 => self.nextToken(),
+                else => blk: {
+                    token.kind = Token.Kind.Symbol;
+                    break :blk token;
+                },
+            };
+        }
+
+        return token;
     }
 };
 
