eddde01991c496757ceeacfb3a3566730bf6838a
Isaac Hier <isaachier@gmail.com>
2018-06-24 15:18:44 -0400

    Avoid memory corruption

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const Builder = @import("std").build.Builder;
 
-pub fn build(b: &Builder) void {
+pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("YOUR_NAME_HERE", "src/main.zig");
+    const exe = b.addExecutable("parse", "src/main.zig");
     exe.setBuildMode(mode);
 
     b.default_step.dependOn(&exe.step);
--- src/main.zig
+++ src/main.zig
@@ -9,32 +9,25 @@ const ArrayList = std.ArrayList;
 const HashMap = std.HashMap;
 
 const Production = struct {
-    name: []const u8,
-    symbols: ArrayList([]const u8),
+    allocator: *Allocator,
+    id: usize,
+    symbols: [][]const u8,
 
-    pub fn init(allocator: *Allocator, name: []const u8) Production {
+    pub fn init(allocator: *Allocator, id: usize, symbols: [][]const u8) Production {
         return Production{
-            .name = name,
-            .symbols = ArrayList([]const u8).init(allocator),
+            .allocator = allocator,
+            .id = id,
+            .symbols = symbols,
         };
     }
 
     pub fn deinit(self: *Production) void {
-        for (self.symbols.items) |symbol| {
-            self.symbols.allocator.free(symbol);
-        }
-        self.symbols.deinit();
-    }
-
-    pub fn append(self: *Production, symbol: []const u8) !void {
-        const symbol_copy = try mem.dupe(self.symbols.allocator, u8, symbol);
-        errdefer self.symbols.allocator.free(symbol_copy);
-        try self.symbols.append(symbol_copy);
+        self.allocator.free(self.symbols);
     }
 };
 
 const RuleSet = struct {
-    const RuleMap = HashMap([]const u8, ArrayList(Production), mem.hash_slice_u8, mem.eql_slice_u8);
+    const RuleMap = HashMap([]const u8, []Production, mem.hash_slice_u8, mem.eql_slice_u8);
 
     const ErrorSet = error {
         RuleDoesNotExist,
@@ -52,32 +45,33 @@ const RuleSet = struct {
 
     pub fn deinit(self: *RuleSet) void {
         var it = self.map.iterator();
-        while (it.next()) |next| {
-            self.map.allocator.free(next.key);
-            next.value.deinit();
+        while (it.next()) |rule| {
+            self.map.allocator.free(rule.key);
+            for (rule.value) |_, i| {
+                rule.value[i].deinit();
+            }
+            self.map.allocator.free(rule.value);
         }
         self.map.deinit();
     }
 
-    pub fn put(self: *RuleSet, name: []const u8, productions: *const ArrayList(Production)) !void {
+    pub fn put(self: *RuleSet, name: []const u8, productions: []Production) !void {
         if (self.map.contains(name)) {
             return;
         }
-        const name_copy = try mem.dupe(self.map.allocator, u8, name);
-        errdefer self.map.allocator.free(name_copy);
-        const old_entry = try self.map.put(name_copy, productions);
-        debug.assert(old_entry == null);
         if (self.start_symbol.len == 0) {
-            self.start_symbol = name_copy;
+            self.start_symbol = name;
         }
+        const old_entry = try self.map.put(name, productions);
+        debug.assert(old_entry == null);
     }
 
-    pub fn get(self: *RuleSet, name: []const u8) ![]const Production {
+    pub fn get(self: *const RuleSet, name: []const u8) ![]const Production {
         var rule_entry = self.map.get(name);
         if (rule_entry == null) {
             return RuleSet.ErrorSet.RuleDoesNotExist;
         }
-        return rule_entry.?.value.toSliceConst();
+        return rule_entry.?.value;
     }
 };
 
@@ -124,6 +118,11 @@ const TokenStream = struct {
         return byte == ' ' or mem.indexOfScalar(u8, cstr.line_sep, byte) != null;
     }
 
+    fn isReserved(byte: u8) bool {
+        const reserved_letters = ";|()[]{}";
+        return mem.indexOfScalar(u8, reserved_letters, byte) != null;
+    }
+
     fn skipSpace(self: *TokenStream) void {
         while (self.pos < self.input.len) : (self.pos += 1) {
             const byte = self.input[self.pos];
@@ -140,6 +139,9 @@ const TokenStream = struct {
         while (self.pos < self.input.len) {
             const byte = self.input[self.pos];
             if (isSpace(byte)) {
+                break;
+            }
+            if (isReserved(byte) and token.pos < self.pos) {
                 break;
             }
             self.pos += 1;
@@ -177,12 +179,14 @@ const Parser = struct {
     token_stream: TokenStream,
     token: Token,
     rule_set: RuleSet,
+    rule_counter: usize,
 
     pub fn init(allocator: *Allocator, input: []const u8) Parser {
         return Parser{
             .token_stream = TokenStream.init(input),
             .token = Token.init(0, 0),
             .rule_set = RuleSet.init(allocator),
+            .rule_counter = 0,
         };
     }
 
@@ -196,7 +200,7 @@ const Parser = struct {
 
     pub fn parse(self: *Parser) !void {
         self.consume();
-        while (self.token.id != Token.Id.Invalid) {
+        while (self.token.id != Token.Id.Invalid) : (self.consume()) {
             try self.parseRule();
         }
     }
@@ -208,7 +212,7 @@ const Parser = struct {
         const name = self.token.slice(self.token_stream.input);
         var productions = ArrayList(Production).init(self.rule_set.map.allocator);
         defer productions.deinit();
-        var production = Production.init(self.rule_set.map.allocator, name);
+        var symbols = ArrayList([]const u8).init(self.rule_set.map.allocator);
 
         self.consume();
         if (self.token.id != Token.Id.Equal) {
@@ -219,14 +223,25 @@ const Parser = struct {
         while (true) : (self.consume()) {
             switch (self.token.id) {
                 Token.Id.Symbol, Token.Id.String =>
-                    try production.append(self.token.slice(self.token_stream.input)),
-                Token.Id.Or => {
-                    try productions.append(production);
-                    production = Production.init(self.rule_set.map.allocator, name);
-                },
-                Token.Id.EndOfRule => {
-                    try productions.append(production);
-                    try self.rule_set.put(name, productions);
+                    try symbols.append(self.token.slice(self.token_stream.input)),
+                Token.Id.Or, Token.Id.EndOfRule => {
+                    {
+                        const symbols_slice = symbols.toOwnedSlice();
+                        errdefer self.rule_set.map.allocator.free(symbols_slice);
+                        try productions.append(Production{
+                            .allocator = self.rule_set.map.allocator,
+                            .id = self.rule_counter,
+                            .symbols = symbols_slice,
+                        });
+                        debug.assert(symbols_slice.len == productions.at(productions.len - 1).symbols.len);
+                        self.rule_counter += 1;
+                    }
+                    if (self.token.id == Token.Id.EndOfRule) {
+                        const productions_slice = productions.toOwnedSlice();
+                        errdefer self.rule_set.map.allocator.free(productions_slice);
+                        try self.rule_set.put(name, productions_slice);
+                        return;
+                    }
                 },
                 Token.Id.Invalid => return,
                 else => return ErrorSet.InvalidToken,
@@ -235,9 +250,79 @@ const Parser = struct {
     }
 };
 
+fn hashUSize(x: usize) u32 {
+    const array = []usize{ x };
+    const slice = array[0..];
+    return mem.hash_slice_u8(@sliceToBytes(slice));
+}
+
+fn eqlUSize(lhs: usize, rhs: usize) bool {
+    return lhs == rhs;
+}
+
+const Item = struct {
+    allocator: *Allocator,
+    production: *const Production,
+    marker: usize,
+
+    pub fn init(allocator: *Allocator, production: *const Production, marker: usize) Item {
+        debug.assert(marker <= production.symbols.len);
+        return Item{
+            .allocator = allocator,
+            .production = production,
+            .marker = marker,
+        };
+    }
+
+    const VisitedSet = HashMap(usize, bool, hashUSize, eqlUSize);
+
+    const ErrorSet = error {
+        OutOfMemory,
+    };
+
+    pub fn findClosure(self: *const Item, rule_set: *const RuleSet) ![]Item {
+        var items = ArrayList(Item).init(self.allocator);
+        var visited = VisitedSet.init(self.allocator);
+        defer visited.deinit();
+        try self.findClosureHelper(rule_set, &items, &visited);
+        return items.toOwnedSlice();
+    }
+
+    fn findClosureHelper(self: *const Item,
+                         rule_set: *const RuleSet,
+                         items: *ArrayList(Item),
+                         visited: *VisitedSet) ErrorSet!void {
+        if (visited.contains(self.production.id)) {
+            return;
+        }
+
+        try items.append(self.*);
+        const old_entry = try visited.put(self.production.id, true);
+        debug.assert(old_entry == null);
+
+        debug.assert(self.marker <= self.production.symbols.len);
+        if (self.marker == self.production.symbols.len) {
+            return;
+        }
+
+        const marked_symbol = self.production.symbols[self.marker];
+        const marked_productions = rule_set.get(marked_symbol) catch []const Production{};
+
+        if (marked_productions.len == 0) {
+            return;
+        }
+
+        for (marked_productions) |marked_production| {
+            const item = Item.init(self.allocator, marked_production, 0);
+            try item.findClosureHelper(rule_set, items, visited);
+        }
+    }
+};
+
 test "parse input" {
     const input =
-    \\ E = E "*" B | E "+" B | B | "0" | "1" ;
+    \\ S = E;
+    \\ E = E "*" B | E "+" B | B | "0" | "1";
     \\
     ;
     var parser = Parser.init(debug.global_allocator, input);
@@ -246,10 +331,13 @@ test "parse input" {
     const productions = try parser.rule_set.get("E");
     debug.assert(productions.len == 5);
     for (productions) |prod| {
-        debug.warn("{} =", prod.name);
-        for (prod.symbols.toSliceConst()) |symbol| {
+        debug.warn("{} =", prod.id);
+        for (prod.symbols) |symbol| {
             debug.warn(" {}", symbol);
         }
         debug.warn("\n");
     }
+    debug.assert(mem.eql_slice_u8(parser.rule_set.start_symbol, "S"));
+    const start_productions = try parser.rule_set.get(parser.rule_set.start_symbol);
+    debug.assert(start_productions.len == 1);
 }
